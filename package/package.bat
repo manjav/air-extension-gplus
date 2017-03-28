9d12b897361a82a9030fc54e89171c25cf66c571
Mansour Djawadi <mansurjavadi@gmail.com>
2017-03-28 15:09:09 +0430

    added ane package dependencies

---
+++ package/package.bat
@@ -0,0 +1,2 @@
+
+adt -package -storetype pkcs12 -keystore cert.p12 -storepass ****** -target ane gplus-extension.ane extension.xml -swc gplus_extension_as3_lib.swc -platform Android-ARM -platformoptions platformoptions.xml -C android . -platform default -C default .
